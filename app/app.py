4063cda17bbdc56a3957e71a6340c5c6657988ec
robarros <robarros@gmail.com>
2025-10-25 00:55:17 -0300

    adjust docker

--- app/app.py
+++ app/app.py
@@ -38,4 +38,4 @@ def readiness_check():
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     debug = os.getenv('DEBUG', 'False').lower() == 'true'
-    app.run(host='0.0.0.0', port=port, debug=debug)
+    app.run(host='0.0.0.0', port=port)
